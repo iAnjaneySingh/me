c9c900002e67d61d811ad0c77405074d9f5f88ee
Anjaney Singh <asinghbits5@gmail.com>
2020-09-10 00:07:05 +0530

    Updates

---
+++ precache-manifest.bbe8b3cf4e6e4f4c9e013356846090d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b52876fb7a6aafe386a942d646fd07b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f91bd81b5a27386b431d",
+    "url": "./static/css/main.cfc4f5b1.chunk.css"
+  },
+  {
+    "revision": "5fab48f80cffba24c530",
+    "url": "./static/js/2.45cb71ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.45cb71ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91bd81b5a27386b431d",
+    "url": "./static/js/main.2cad401a.chunk.js"
+  },
+  {
+    "revision": "5a9ca95dfee7036cfe1b",
+    "url": "./static/js/runtime-main.6872adf3.js"
+  },
+  {
+    "revision": "faff043942c03e39c81af574e5b1262e",
+    "url": "./static/media/5.faff0439.jpg"
+  }
+]);
